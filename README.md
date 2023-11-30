6d9769e5a78ac1696394afbf3116767452215afe
Art <art@riptidewave.com>
2023-11-30 10:00:42 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Advent of code 2023
+
+## Advent of Code project template
+
+I used (this template)[https://github.com/agubelu/AoC-rust-template] as a starting point for this year.
---
+++ src/days/day06.rs
@@ -0,0 +1,33 @@
+use crate::{Solution, SolutionPair};
+
+fn part_a(_input: &str) -> u32 {
+    0
+}
+
+fn part_b(_input: &str) -> u32 {
+    0
+}
+
+pub fn solve() -> SolutionPair {
+    // Your solution here...
+
+    (Solution::from(part_a("")), Solution::from(part_b("")))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    static TEST_INPUT: &str = "";
+
+    #[test]
+    fn test_part_a() {
+        let result = part_a(TEST_INPUT);
+        assert_eq!(result, 0);
+    }
+
+    #[test]
+    fn test_part_b() {
+        let result = part_b(TEST_INPUT);
+        assert_eq!(result, 0);
+    }
+}
