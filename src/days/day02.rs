8095c9f9bce65088548681e26ec827e36999d40b
Art <art@riptidewave.com>
2023-12-02 17:40:27 +0000

    Add comments

--- src/days/day02.rs
+++ src/days/day02.rs
@@ -15,13 +15,17 @@ struct Game {
 }
 
 impl Game {
+    // Implement a function to determine if a valid nuber of cubes were used within the game
     fn is_valid(&self) -> bool {
         self.red <= 12 && self.green <= 13 && self.blue <= 14
     }
 }
 
 fn parse_game(input: &str) -> IResult<&str, Game> {
+    // Separate and parse all the different rounds within the game
     let (input, data) = separated_list0(tag(", "), separated_pair(u32, space1, alpha1))(input)?;
+
+    // Todo: Maybe use map_res? this also works though
     let mut game = Game::default();
     for (count, colour) in data {
         match colour {
@@ -35,8 +39,11 @@ fn parse_game(input: &str) -> IResult<&str, Game> {
 }
 
 fn parse_lines(input: &str) -> IResult<&str, Vec<Game>> {
+    // Split of the start of the input as we don't need it
     let (input, _) = take_till(|c| c == ':')(input)?;
     let (input, _) = take(2_u8)(input)?;
+
+    // Parse the rest as a list of games
     separated_list0(tag("; "), parse_game)(input)
 }
 
@@ -45,9 +52,12 @@ fn part_a(input: &str) -> u32 {
         .lines()
         .map(|line| {
             let (_, data) = parse_lines(line).unwrap();
+            // Parse the input and return a boolean defined by whether all the rounds were valid
             data.iter().all(|game| game.is_valid())
         })
+        // Use enumerate to get the game number
         .enumerate()
+        // Map to preserve index
         .map(|(i, e)| if e { i as u32 + 1 } else { 0 })
         .sum::<u32>()
 }
@@ -57,6 +67,7 @@ fn part_b(input: &str) -> u32 {
         .lines()
         .map(|line| {
             let (_, data) = parse_lines(line).unwrap();
+            // Fold over the rounds to find the max values for red, green and blue
             let (max_red, max_green, max_blue) = data.iter().fold((0, 0, 0), |(r, g, b), game| {
                 (r.max(game.red), g.max(game.green), b.max(game.blue))
             });
